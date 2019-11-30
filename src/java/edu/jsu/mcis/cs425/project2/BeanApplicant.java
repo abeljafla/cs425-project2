0de73059c2450aaf1efe290028f1a8927f6dedd6
abeljafla <43124544+abeljafla@users.noreply.github.com>
2019-11-30 17:23:10 -0600

    Update BeanApplicant.java

--- src/java/edu/jsu/mcis/cs425/project2/BeanApplicant.java
+++ src/java/edu/jsu/mcis/cs425/project2/BeanApplicant.java
@@ -1,8 +1,34 @@
 package edu.jsu.mcis.cs425.project2;
 
+import java.util.HashMap;
+
 public class BeanApplicant {
     
-    private String username;
+   private String username;
+   private String displayname;
+    private int userid;
+    private String[] skills;
+   private String[] jobs;
+   
+   public String getSkillsList(){
+        Database db = new Database();
+        return (db.getSkillsListAsHTML(userid));
+    }
+    
+   public void setUserInfo(){
+      Database db = new Database();
+       HashMap<String, String> userinfo = db.getUserInfo(username);
+        userid = Integer.parseInt(userinfo.get("userid"));
+        displayname = userinfo.get("displayname");
+    }
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
 
     public String getUsername() {
         return username;
@@ -12,4 +38,30 @@ public class BeanApplicant {
         this.username = username;
     }
     
+    public String getJobsList() {
+        Database db = new Database();
+        return ( db.getJobsListAsHTML(userid) );
+    }
+    
+    public String[] getSkills(){
+        return skills;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+    
+    public void setSkillsList(){
+        Database db = new Database();
+        //db.setSkillsList(userid, skills);
+     }
+    
+    public void setJobsList() {
+        Database db = new Database();
+        //db.setJobsList(userid, jobs);
+     }
 }
