8b547f7814b0d872428538aa4194c279b6e9a94c
abeljafla <43124544+abeljafla@users.noreply.github.com>
2019-12-03 17:14:56 -0600

    latest updated

--- src/java/edu/jsu/mcis/cs425/project2/Database.java
+++ src/java/edu/jsu/mcis/cs425/project2/Database.java
@@ -15,7 +15,8 @@ public class Database {
     public HashMap getUserInfo(String username){
         
         HashMap<String, String> results = null;
-                
+               
+        
         try {
         
             Connection conn = getConnection();
@@ -23,33 +24,26 @@ public class Database {
             String query = "SELECT * FROM user WHERE username = ?";
             
             PreparedStatement pstatement = conn.prepareStatement(query);
-            pstatement.setString(1, username);
-            
-            boolean hasresults = pstatement.execute();            
-                
+            pstatement.setString(1, username);            
+            boolean hasresults = pstatement.execute();                           
             if ( hasresults ) {
-                ResultSet resultset = pstatement.getResultSet();
-                
+                ResultSet resultset = pstatement.getResultSet();            
                 if(resultset.next()){
                     //Initalize HashMap; add user data from resultset
-                    //use descriptive key name "id" for the ID, and "displayname" for the displayname
-                    
+                    //use descriptive key name "id" for the ID, and "displayname" for the displayname               
                     results = new HashMap<>();
                     String id = String.valueOf((resultset.getInt("id")));
                     String displayname = resultset.getString("displayname");
                     results.put("userid", id);
                     results.put("displayname", displayname);
-                   // results += getResultSetTable(resultset);
-                    
+                   // results += getResultSetTable(resultset);                
                     
                 }
             }
 
         }
-          catch (Exception e) { e.printStackTrace(); }
-        
-        return results;
-        
+          catch (Exception e) { e.printStackTrace(); }      
+        return results;   
         
     }
     
@@ -67,26 +61,20 @@ public class Database {
                 Connection conn = getConnection();
                 PreparedStatement pstatement = conn.prepareStatement(query);
                 pstatement.setInt(1, userid);
-
                 boolean hasresults = pstatement.execute(); 
-
                 if(hasresults){
                     ResultSet resultset = pstatement.getResultSet();
-
                     while(resultset.next()){
                         String description = resultset.getString("description");
                         int id = resultset.getInt("id");
                         int user = resultset.getInt("userid");
-
                         s.append("<input type=\"checkbox\" name=\"skills\" value=\"");
                         s.append(id);
                         s.append("\" id=\"skills_").append(id).append( "\" ");
-
                         // is this box checked?
                         if(user != 0){
                             s.append("checked");
                         }
-
                         s.append(">\n");
 
                         s.append("<label for=\"skills_").append(id).append("\">");
@@ -98,16 +86,9 @@ public class Database {
                     }
                 }
             }     
-            catch(Exception e){ e.printStackTrace(); }
-        
+            catch(Exception e){ e.printStackTrace(); }    
         return s.toString();
-    }
-    
-    //SELECT a.userid, j.name, s.skillsid FROM ((applicants_to_skills a JOIN skills_to_jobs s ON a.skillsid = s.skillsid)
-    //JOIN jobs j on s.jobsid = j.id);
-
-
-    
+    }   
     public String getJobsListAsHTML(int userid) {
         StringBuilder s = new StringBuilder();
         
@@ -115,11 +96,15 @@ public class Database {
         //throw an exception if there is an error in the database
         
         //String query = "SELECT * FROM 'user' WHERE username = ?";
-        String query = "SELECT jobs.id, jobs.name, a.userid FROM jobs LEFT JOIN(SELECT * FROM applicants_to_jobs WHERE userid = ?) AS a ON jobs.id = a.jobsid WHERE jobs.id IN(SELECT jobsid AS id FROM(applicants_to_skills JOIN skills_to_jobs ON applicants_to_skills.skillsid = skills_to_jobs.skillsid) WHERE applicants_to_skills.userid = ?) ORDER BY jobs.name";
+        String query = "SELECT jobs.id, jobs.name, a.userid FROM jobs LEFT JOIN(SELECT * "
+                + "FROM applicants_to_jobs WHERE userid = ?) AS a ON jobs.id = a.jobsid WHERE jobs.id IN(SELECT jobsid AS id "
+                + "FROM(applicants_to_skills JOIN skills_to_jobs ON applicants_to_skills.skillsid = skills_to_jobs.skillsid) "
+                + "WHERE applicants_to_skills.userid = ?) ORDER BY jobs.name";
             try {
                 Connection conn = getConnection();
                 PreparedStatement pstatement = conn.prepareStatement(query);
                 pstatement.setInt(1, userid);
+                pstatement.setInt(2, userid);
 
                 boolean hasresults = pstatement.execute(); 
 
@@ -127,7 +112,7 @@ public class Database {
                     ResultSet resultset = pstatement.getResultSet();
 
                     while(resultset.next()){
-                        String description = resultset.getString("description");
+                        String description = resultset.getString("name");
                         int id = resultset.getInt("id");
                         int user = resultset.getInt("userid");
 
@@ -158,35 +143,70 @@ public class Database {
         
         return s.toString();
     }
-    /*
-    public setSkillsList(int userid, String[] skills){
-        //skills = null;
+    
+    public void setSkillsList(int userid, String[] skills){
         try{
             Connection conn = getConnection();
             //conn = DriverManager.getConnection(server, user, password);
-            String query = "UPDATE * FROM 'user' WHERE username = ?";
-            PreparedStatement stmt = conn.prepareStatement(query);
+            
+            System.err.println( Arrays.toString(skills) );
+            
+            // Delete existing skill selections (if any)
+            
+            String deletequery = "DELETE FROM applicants_to_skills WHERE userid = ?";
+            PreparedStatement deletestmt = conn.prepareStatement(deletequery);
+            deletestmt.setInt(1, userid);
+            deletestmt.executeUpdate();
+            
+            // Insert new skill selections
+            
+            String insertquery = "INSERT INTO applicants_to_skills (userid, skillsid) VALUES (?, ?)";
+            PreparedStatement insertstmt = conn.prepareStatement(insertquery);
+            
             //add new selected skills r
+            for(int i = 0; i < skills.length; i++){
+                int skillsid = Integer.parseInt(skills[i]);
+                insertstmt.setInt(1, userid);
+                insertstmt.setInt(2, skillsid);
+                insertstmt.addBatch();
+            }
+            
+            int[] results = insertstmt.executeBatch();
+            deletestmt.close();
+            insertstmt.close();       
         }
         catch(Exception e){ e.printStackTrace(); }
     }
-
-    public setJobsList(int userid, String[] skills){
-        skills = null;
-    }
-*/
-    
-    public Connection getConnection(){
-        
-       Connection conn = null;
-       
-        try {
+    public void setJobsList(int userid, String[] jobs){
+        try{
+            Connection conn = getConnection();
+            //conn = DriverManager.getConnection(server, user, password);
+            String deletequery = "DELETE FROM applicants_to_jobs WHERE userid =?";
+            PreparedStatement deletestmt = conn.prepareStatement(deletequery);
+            String insertquery = "INSERT * FROM applicants_to_jobs a";
+            PreparedStatement insertstmt = conn.prepareStatement(insertquery);
+            //add new selected skills r
+            for(int i =0; i < jobs.length; i++){
+                
+                insertstmt.addBatch();
+            }
             
+            int[] d = deletestmt.executeBatch();
+            int[] i = insertstmt.executeBatch();
+            conn.commit();
+            deletestmt.close();
+            insertstmt.close();
+        }
+        catch(Exception e){ e.printStackTrace(); }
+    }   
+    public Connection getConnection(){        
+       Connection conn = null       
+        try 
             Context envContext = new InitialContext();
             Context initContext  = (Context)envContext.lookup("java:/comp/env");
             DataSource ds = (DataSource)initContext.lookup("jdbc/db_pool");
             conn = ds.getConnection();
-            
+           
         }        
         catch (Exception e) {e.printStackTrace();}
         
